0912e591c594934905ff2d8c90303d873efd4f60
Adnan Shaikh <aa2232783@gmail.com>
2022-08-13 18:21:31 +0530

    Created University and State Models

--- uid/core/models.py
+++ uid/core/models.py
@@ -1,3 +1,50 @@
+
+import re
 from django.db import models
 
 # Create your models here.
+
+class StateManager(models.Manager):
+    
+    def create_state(self,state_name):
+        state_name = state_name.lower().replace(" ","")
+        state = self.create(state_name=state_name)
+        return state
+class State(models.Model):
+    
+    state_name = models.CharField(max_length=30, null=False,editable=False)
+    state_id = models.AutoField(primary_key=True)
+    objects = StateManager()
+    def __str__(self):
+        return str(self.state_name)
+
+class UniversityManager(models.Manager):
+    
+    def create_university(self,uni_name,state_name,state_level_id):
+        state = State.objects.get(state_name=state_name.lower().replace(" ",""))
+        uni_id = int(str(state.state_id)+str(state_level_id))   
+        university = self.create(
+            uni_name = uni_name.lower().replace(" ",""),
+            state = state,
+            state_level_id=state_level_id,
+            uni_id=uni_id
+        )
+        return university
+class University(models.Model):
+    
+    uni_name = models.CharField(max_length=50,null=False)
+    state = models.ForeignKey(State,related_name="state",on_delete=models.DO_NOTHING,editable=False)
+    state_level_id = models.IntegerField(null=False,editable=False)
+    uni_id = models.IntegerField(primary_key=True,editable=False)
+    objects = UniversityManager()
+    
+    def __str__(self):
+        return str(self.uni_name)
+    
+class College(models.Model):
+    
+    college_name = models.CharField(max_length=50,null=False)
+    college_type = models.CharField(max_length=20,null=False)
+    university = models.ForeignKey(University,on_delete=models.DO_NOTHING) 
+    uni_level_id=models.IntegerField(null=False)
+    
--- uid/uid/settings.py
+++ uid/uid/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
 
+from pathlib import Path
+from constants.ConstVarible import db_config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,10 +81,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'uid',
-        'USER': 'root',
-        'PASSWORD': 'admin',
+        'USER': db_config["USER"],
+        'PASSWORD': db_config["PASSWORD"],
         'HOST': 'localhost',
-        'PORT': '3306'
+        'PORT': db_config["PORT"]
     }
 }
 
